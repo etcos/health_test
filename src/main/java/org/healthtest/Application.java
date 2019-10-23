992514c451eb470cdc17fe750610f0881f814c12
Evgeniy Panov <e.panov@fundcount.com>
2019-10-23 10:43:43 +0300

    hibernate core

--- src/main/java/org/healthtest/Application.java
+++ src/main/java/org/healthtest/Application.java
@@ -2,8 +2,9 @@ package org.healthtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.*;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
